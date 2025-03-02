4214012dc4e12df5dbd843e5966db5581cb3cf2c
Sergey <shornikov1407@gmail.com>
2025-03-02 15:27:11 +0300

    update processHandlers, test process spawn

--- electron-app/src/ipcHandlers/processHandler.js
+++ electron-app/src/ipcHandlers/processHandler.js
@@ -1,14 +1,15 @@
 const { spawn } = require("child_process");
 const treeKill = require("tree-kill");
+const {getSettings} = require("../utils/fsHelper");
 
 const processes = {};
 
 function initializeProcessHandlers(ipcMain, mainWindow) {
     ipcMain.on("start-process", (event, { taskId, config }) => {
         console.log(`Создан taskId: ${taskId}, запускаем процесс...`);
-
+        const scriptPath = getSettings();
         event.reply("process-started", { taskId, config });
-
+        // const child = spawnProcess(config, scriptPath.scriptDirectory);
         const child = spawn("node", ["your_script.js"]);
         processes[taskId] = child;
 
@@ -26,6 +27,31 @@ function initializeProcessHandlers(ipcMain, mainWindow) {
             mainWindow?.webContents.send("process-exit", { taskId, code });
         });
     });
+    // Возобновление процесса
+    ipcMain.on("resume-process", (event, { taskId, config }) => {
+        console.log(`Resume-process: Task ${taskId}, config:`, config);
+
+        event.reply("process-started", { taskId, config });
+        const scriptPath = getSettings();
+        // const child = spawnProcess(config, scriptPath.scriptDirectory);
+        const child = spawn("node", ["your_script.js"]);
+        processes[taskId] = child;
+
+        child.stdout.on("data", (data) => {
+            console.log(`STDOUT [Task ${taskId}]:`, data.toString());
+            mainWindow?.webContents.send("process-output", { taskId, log: data.toString() });
+        });
+
+        child.stderr.on("data", (data) => {
+            console.error(`STDERR [Task ${taskId}]:`, data.toString());
+        });
+
+        child.on("exit", (code) => {
+            console.log(`Процесс Task ${taskId} (resume) завершился с кодом ${code}`);
+            mainWindow?.webContents.send("process-exit", { taskId, code });
+        });
+    });
+
 
     ipcMain.on("stop-process", (event, taskId) => {
         const child = processes[taskId];
