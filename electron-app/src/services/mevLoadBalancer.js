7089e044f74f85236a5f25f26d84cce46b711943
Sergey <shornikov1407@gmail.com>
2025-04-08 13:26:52 +0300

    delay 3

--- electron-app/src/services/mevLoadBalancer.js
+++ electron-app/src/services/mevLoadBalancer.js
@@ -1026,7 +1026,7 @@ class MevLoadBalancer {
         if (processId) {
           newProcesses.push(processId);
         }
-        await sleep(1000);
+        await sleep(3000);
       }
 
       // Шаг 7: Перезапускаем все сохраненные процессы с новой задержкой
@@ -1037,7 +1037,7 @@ class MevLoadBalancer {
         if (restartedProcessId) {
           restartedProcesses.push(restartedProcessId);
         }
-        await sleep(1000);
+        await sleep(3000);
       }
 
       console.log(`[MEV LoadBalancer] Перезапущено ${restartedProcesses.length} из ${processConfigs.length} процессов`);
