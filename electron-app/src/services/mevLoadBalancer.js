24b2004c7362b4c82a18e242820b2a60c32239ee
Sergey <shornikov1407@gmail.com>
2025-04-14 23:13:51 +0300

    back jito and delay

--- electron-app/src/services/mevLoadBalancer.js
+++ electron-app/src/services/mevLoadBalancer.js
@@ -415,11 +415,11 @@ class MevLoadBalancer {
     async startMevProcess(config) {
         try {
             console.log(`ckeck token exists on "${config.tokenAddress}"`);
-            // if (!this.userTokens.has(config.tokenAddress.trim())) {
-            //     console.log(`NO TOKEN ACCOUNT, CREATING...`)
-            //     this.userTokens.set(config.tokenAddress.trim(), await createTokenAccount(this.userSettings.mainRpc, config.tokenAddress.trim(), this.USER, this.userTokens));
-            //     await sleep(30000);
-            // }
+            if (!this.userTokens.has(config.tokenAddress.trim())) {
+                console.log(`NO TOKEN ACCOUNT, CREATING...`)
+                this.userTokens.set(config.tokenAddress.trim(), await createTokenAccount(this.userSettings.mainRpc, config.tokenAddress.trim(), this.USER, this.userTokens));
+                await sleep(10000);
+            }
 
 
             // Проверяем обязательные параметры
