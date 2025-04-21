6c449192e37c8420959b74c4e8841dd9b85e0840
Sergey <shornikov1407@gmail.com>
2025-04-21 23:22:55 +0300

    ---

--- electron-app/src/utils/solanaUtils.js
+++ electron-app/src/utils/solanaUtils.js
@@ -654,7 +654,7 @@ async function getDetailedTokenAccounts(ownerPubkey, rpcUrl) {
 
     const detailedAccounts = await Promise.all(
         response.value.map(async ({pubkey}) => {
-            await sleep(100);
+            await sleep(200);
             const accountInfo = await getAccount(connection, pubkey);
             return {
                 address: pubkey.toBase58(),
