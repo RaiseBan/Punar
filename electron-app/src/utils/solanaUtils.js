2bf5ed2aff1f112c464799c4645bbcc200de57ab
Sergey <shornikov1407@gmail.com>
2025-04-22 12:30:27 +0300

    up delay

--- electron-app/src/utils/solanaUtils.js
+++ electron-app/src/utils/solanaUtils.js
@@ -654,7 +654,7 @@ async function getDetailedTokenAccounts(ownerPubkey, rpcUrl) {
 
     const detailedAccounts = await Promise.all(
         response.value.map(async ({pubkey}) => {
-            await sleep(200);
+            await sleep(400);
             const accountInfo = await getAccount(connection, pubkey);
             return {
                 address: pubkey.toBase58(),
