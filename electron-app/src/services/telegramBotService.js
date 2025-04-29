cae156ffc74f38bf1b7380b51a4cbe8d300a3684
Sergey <shornikov1407@gmail.com>
2025-04-30 02:57:03 +0300

    fix /get_add_dump

--- electron-app/src/services/telegramBotService.js
+++ electron-app/src/services/telegramBotService.js
@@ -1565,7 +1565,7 @@ class TelegramBotService {
                     message += `<b>============================================</b>\n`;
                     i++;
                 }
-
+                this.sendMessage(chatId, message);
 
             }catch(error){
                 console.error('[TG Bot] Ошибка:', error);
