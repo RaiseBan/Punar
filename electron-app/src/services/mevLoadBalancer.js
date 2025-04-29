5a00a7d43c315a2634e2707f98024cc428f256b9
Sergey <shornikov1407@gmail.com>
2025-04-29 15:50:04 +0300

    ---

--- electron-app/src/services/mevLoadBalancer.js
+++ electron-app/src/services/mevLoadBalancer.js
@@ -91,11 +91,13 @@ class MevLoadBalancer {
 
             this.USER = Keypair.fromSecretKey(new Uint8Array(bs58.default.decode(this.userSettings.migration_wallet)));
             // кешируем токен аккаунты
+            console.log(1)
             const tokenObjects = await getDetailedTokenAccounts(this.USER.publicKey, this.userSettings.mainRpc);
-
+            console.log(2)
             tokenObjects.forEach(tokenFields => {
                 this.userTokens.set(tokenFields.mint, tokenFields.address);
             })
+            console.log(3)
 
             // Запускаем таймер обработки сигналов
             this.startProcessingTimer();
@@ -121,6 +123,7 @@ class MevLoadBalancer {
 
         this.processingTimer = setInterval(() => {
             this.processSignalBuffer();
+
         }, this.settings.processingInterval);
     }
 
