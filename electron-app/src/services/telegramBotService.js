d31251e8be7de1e242fc2af5c11d34bacd8b604a
Sergey <shornikov1407@gmail.com>
2025-04-29 15:02:56 +0300

    add command to delete

--- electron-app/src/services/telegramBotService.js
+++ electron-app/src/services/telegramBotService.js
@@ -1436,6 +1436,8 @@ class TelegramBotService {
                     message += `<b>Токен:</b> <code>${proc.tokenAddress}</code>\n`;
                     message += `<b>Meteora пул:</b> <code>${proc.meteoraPool ? proc.meteoraPool : 'не указан'}</code>\n`;
                     message += `<b>Время работы:</b> ${runtime} мин.\n`;
+                    message += `<b>DELETE:</b> /mev_stop_process ${proc.id}`;
+
                 }
 
                 this.sendMessage(chatId, message);
