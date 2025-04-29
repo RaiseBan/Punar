c83fcb9cc81ce8345ce17bc870c411b2ce547c69
Sergey <shornikov1407@gmail.com>
2025-04-29 23:23:07 +0300

    add number for proc out

--- electron-app/src/services/telegramBotService.js
+++ electron-app/src/services/telegramBotService.js
@@ -1525,17 +1525,19 @@ class TelegramBotService {
                 }
 
                 let message = '📊 <b>Активные MEV процессы</b>\n\n';
-
+                let i = 1;
                 for (const proc of processes) {
                     const runtime = Math.floor((Date.now() - proc.startTime) / 1000 / 60); // в минутах
 
-                    message += `<b>ID:</b> <code>${proc.id}</code> (PID: ${proc.pid || 'неизвестно'})\n`;
-                    message += `<b>Token:</b> <code>${proc.tokenAddress}</code>\n`;
-                    message += `<b>Meteora pool:</b> <code>${proc.meteoraPool ? proc.meteoraPool : 'N/A'}</code>\n`;
-                    message += `<b>Meteora pool:</b> <code>${proc.pumpSwapPool ? proc.pumpSwapPool : 'N/A'}</code>\n`;
-                    message += `<b>Uptime:</b> ${runtime} min.\n`;
-                    message += `<b>DELETE:</b> <code>/mev_stop_process ${proc.id}</code>\n`;
+                    message += `<b>${i}. ID:</b> <code>${proc.id}</code> (PID: ${proc.pid || 'неизвестно'})\n`;
+                    message += `<b>${i}. Token:</b> <code>${proc.tokenAddress}</code>\n`;
+                    message += `<b>${i}. Meteora pool:</b> <code>${proc.meteoraPool ? proc.meteoraPool : 'N/A'}</code>\n`;
+                    message += `<b>${i}. Meteora pool:</b> <code>${proc.pumpSwapPool ? proc.pumpSwapPool : 'N/A'}</code>\n`;
+                    message += `<b>${i}. Uptime:</b> ${runtime} min.\n`;
+                    message += `<b>${i}. DELETE:</b> <code>/mev_stop_process ${proc.id}</code>\n`;
                     message += `<b>============================================</b>\n`;
+
+                    i++;
 
                 }
 
