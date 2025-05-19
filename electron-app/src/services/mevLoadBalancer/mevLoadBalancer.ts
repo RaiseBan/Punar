aa5766bf2aade3817b3d220ed414e5219a8c104f
Sergey <shornikov1407@gmail.com>
2025-05-19 16:39:14 +0300

    add lookupTables to config

--- electron-app/src/services/mevLoadBalancer/mevLoadBalancer.ts
+++ electron-app/src/services/mevLoadBalancer/mevLoadBalancer.ts
@@ -1909,10 +1909,11 @@ export class MevLoadBalancer {
                     pumpSwapPool,
                     raydiumPool,
                     meteoraDAMMPool,
-                    type
+                    type,
+                    lookupTables
                 } = signal;
 
-                const struct = {
+                const struct: ProcessConfig = {
                     tokenAddress,
                     meteoraPools: meteoraPools, // Используем обновленную копию
                     pumpSwapPool: pumpSwapPool ? pumpSwapPool : undefined,
@@ -1924,7 +1925,9 @@ export class MevLoadBalancer {
                     jito_lower_bound: Number(this.userSettings!.jito_lower_bound), // deprecated
                     jito_upper_bound: Number(this.userSettings!.jito_upper_bound), // deprecated
                     process_delay: null,
-                    task_name: `mev_task_${Date.now().toString().substring(8, 13)}`
+                    task_name: `mev_task_${Date.now().toString().substring(8, 13)}`,
+                    lookupTables: lookupTables
+
                 };
 
                 newProcessConfigs.push(struct);
